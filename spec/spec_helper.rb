0652970055eaecab4b3d812424e0aeba09e1599f
Even Chang <kiki44552002@gmail.com>
2014-09-14 15:28:25 +0800

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,11 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
 RSpec.configure do |config|
+  require 'capybara/rspec'
+
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -82,4 +86,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.include Capybara::DSL
 end
