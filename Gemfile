bbf7a6364dbfa71864259abe2139d54924cb4f8a
Even Chang <kiki44552002@gmail.com>
2014-09-18 11:19:55 +0800

    Finish user edit, update, index, adn destroy actions

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ ruby '2.1.2'
 gem 'rails', '4.1.5'
 gem 'bootstrap-sass', '~> 3.2.0.2'
 gem 'bcrypt-ruby', '~> 3.1.5'
+gem 'faker', '~> 1.4.3'
+gem 'will_paginate', '~> 3.0.7'
+gem 'bootstrap-will_paginate', '~> 0.0.10'
 
 group :development, :test do
   gem 'sqlite3'
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Authentication" do
   subject { page }
 
   describe "signin page" do
-    before { visit signin_path}
+    before { visit signin_path }
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
@@ -24,14 +24,11 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
@@ -41,5 +38,27 @@ describe "Authentication" do
       end
 
     end
+  end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+
+
   end
 end
